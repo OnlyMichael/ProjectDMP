7c265dfc5b77208ce06efa14860c1a983391a182
MichaelAdebowale <michael.adebowale17324@gmail.com>
2023-05-31 19:08:04 +0200

    Game.cs updated -- eerste klasse is ingevuld + genre

--- models/Game.cs
+++ models/Game.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Printing;
 using System.Text;
 
 namespace models
 {
     public class Game
     {
+        //properties
+        public int Id { get; set; }
+
+        public string Naam { get; set; }
+        public string Beschrijving { get; set; }
+        public int MinimumLeeftijd { get; set; }
+        public Nullable<decimal> Prijs { get; set; }
+        public double Rating { get; set; }
     }
 }
--- models/Genre.cs
+++ models/Genre.cs
@@ -4,5 +4,7 @@ namespace models
 {
     public class Genre
     {
+        public int Id { get; set; }
+        public string Naam { get; set; }
     }
 }
